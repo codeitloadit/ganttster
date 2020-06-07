7c43ff09f02373d7cd5e03150134aa6bbe31aed4
Brian <brian@codeitload.it>
2020-06-06 23:53:23 -0700

    Init

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header class="header">
+        <h1>Ganttster</h1>
+        <div id="nav">
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style scoped>
+.header {
+    background: #333;
+    color: #fff;
+    text-align: center;
+    padding: 10px;
+}
+
+#nav a {
+    font-weight: bold;
+    color: #aaa;
+    padding: 0 15px;
+    text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+    color: #fff;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="about">
+        <h1>About</h1>
+        <p>Coming soon...</p>
+    </div>
+</template>
+
+<style scoped>
+.about {
+    padding: 10px;
+}
+</style>
